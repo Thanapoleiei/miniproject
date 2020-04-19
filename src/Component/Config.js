d9e28eb2f456c4e8c173cfabff1cf5e9dd1fd3c4
Thanapoleiei <loveyouna089@gamil.com>
2020-04-19 22:14:34 +0700

    firebase

---
+++ src/Component/Config.js
@@ -0,0 +1,12 @@
+const Config = {
+    apiKey: "AIzaSyBjNodXG59L-LiKI_uUdjgpxKx6_jCwdKc",
+    authDomain: "miniproject-client.firebaseapp.com",
+    databaseURL: "https://miniproject-client.firebaseio.com",
+    projectId: "miniproject-client",
+    storageBucket: "miniproject-client.appspot.com",
+    messagingSenderId: "526330409178",
+    appId: "1:526330409178:web:c76b05ca13b6b501c035e3",
+    measurementId: "G-JDXPEPS05Q"
+  };
+
+  export default Config;
